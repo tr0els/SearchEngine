fade702ab3e6d194e278e006d23bc9178716df5f
Troels <31724037+tr0els@users.noreply.github.com>
2022-03-18 17:53:48 +0100

    Gateway working with search

--- GatewayAPI/Controllers/GatewayController.cs
+++ GatewayAPI/Controllers/GatewayController.cs
@@ -1,77 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace GatewayAPI.Controllers
 {
+    /*
+     * Gateway calls microservices and returns response back using http
+     */
+
     [Route("[action]")]
     [ApiController]
+
     public class ProxyController : ControllerBase
     {
         private readonly HttpClient _httpClient;
+
+        // Injecting httpClientFactory from app service
         public ProxyController(IHttpClientFactory httpClientFactory)
         {
             _httpClient = httpClientFactory.CreateClient();
         }
-        [HttpGet]
-        public async Task<IActionResult> Books()
-            => await ProxyTo("https://localhost:44388/books");
 
-        /*
         [HttpGet]
-        public async Task<IActionResult> Authors()
-            => await ProxyTo("https://localhost:44307/authors");
-        */
+        [Route("{query}/{maxAmount}")]
+        public async Task<IActionResult> Search(string query, int maxAmount)
+        {
+            return await ProxyTo("https://localhost:44307/search/" + query + "/" + maxAmount);
+        }
 
         private async Task<ContentResult> ProxyTo(string url)
             => Content(await _httpClient.GetStringAsync(url));
     }
-
-
-
-
-
-
-    /*
-    [Route("api/[controller]")]
-    [ApiController]
-    public class GatewayController : ControllerBase
-    {
-        //private static SearchLogic searchLogic = new SearchLogic(new Database());
-
-        // GET: api/<GatewayController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<GatewayController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<GatewayController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<GatewayController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<GatewayController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
-    */
 }
 
 
