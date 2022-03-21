f949b89293dcf2967f78ff0b0d4be9f7d06e46ba
Troels <31724037+tr0els@users.noreply.github.com>
2022-03-21 23:23:26 +0100

    Update App.cs

--- ConsoleDidYouMean/App.cs
+++ ConsoleDidYouMean/App.cs
@@ -12,12 +12,13 @@ namespace ConsoleDidYouMean
             
             while (true)
             {
-                Console.WriteLine("enter a word - q for quit");
+                Console.WriteLine("Press enter to start sending requests to load balancer - q for quit");
                 string input = Console.ReadLine();
                 if (input.Equals("q")) break;
 
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < 1000; i++)
                 {
+                    Task.Delay(1).Wait();
                     GetUrl(input);
                 }
 
@@ -27,14 +28,14 @@ namespace ConsoleDidYouMean
 
         public async void GetUrl(string input)
         {
-            Console.WriteLine($"Console sending request to load balancer at {DateTime.Now.TimeOfDay}");
+            Console.WriteLine($"{DateTime.Now.TimeOfDay} - Console sending request to load balancer");
 
             Task<string> getStringTask = _client.GetStringAsync("https://localhost:44306/DidYouMean");
             
             var content = await getStringTask;
             var result = JsonConvert.DeserializeObject<DidYouMean>(content);
             
-            Console.WriteLine($"Console got response from load balancer at {DateTime.Now.TimeOfDay}");
+            Console.WriteLine($"{DateTime.Now.TimeOfDay} - Console got response from load balancer");
 
             //return result;
         }
