16321fee2474849f3cc0350017c4ffe244ae5862
Troels <31724037+tr0els@users.noreply.github.com>
2022-03-21 23:23:45 +0100

    Update ILoadBalancer.cs

--- LoadBalancerAPI/Controllers/ILoadBalancer.cs
+++ LoadBalancerAPI/Controllers/ILoadBalancer.cs
@@ -2,6 +2,12 @@
 {
     public interface ILoadBalancer
     {
+        public List<string> GetAllServices();
+        public int AddService(string url);
+        public int RemoveService(int id);
+        public Dictionary<string, ILoadBalancerStrategy> GetAllStrategies();
+        public ILoadBalancerStrategy GetActiveStrategy();
+        public void SetActiveStrategy(string strategyName);
         public string NextService();
     }
 }
